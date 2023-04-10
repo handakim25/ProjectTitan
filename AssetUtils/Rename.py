100e9ab065510d158d169c249904b9891d24bcc7
handakim25 <handakim25@gmail.com>
2023-04-10 11:49:22 +0900

    Fixed a typo in Rename.py

--- AssetUtils/Rename.py
+++ AssetUtils/Rename.py
@@ -2,9 +2,9 @@ import os
 import glob
 
 files = glob.glob('*.png')
-suffx = '_icon'
+suffix = '_icon'
 
 for full_file_name in files:
     file_name, file_ext = os.path.splitext(full_file_name)
-    new_file_name = file_name + suffx + file_ext
+    new_file_name = file_name + suffix + file_ext
     os.rename(full_file_name, new_file_name)
