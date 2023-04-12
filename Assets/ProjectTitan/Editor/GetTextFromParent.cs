65716944e2111f2eb1da22271512648ab3c5f15a
handakim25 <handakim25@gmail.com>
2023-04-12 18:00:11 +0900

    Still doing

---
+++ Assets/ProjectTitan/Editor/GetTextFromParent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Titan.Editor
+{
+    public class GetTextFromParent : MonoBehaviour
+    {
+        /// <summary>
+        /// Called when the script is loaded or a value is changed in the
+        /// inspector (Called in the editor only).
+        /// </summary>
+        private void OnValidate()
+        {
+            // transform.parent
+        }
+    }
+}
