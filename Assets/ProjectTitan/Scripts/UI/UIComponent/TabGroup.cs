b7790c0e1d9a21d1394736edc3405e27d7d16860
handakim25 <handakim25@gmail.com>
2023-04-15 17:02:26 +0900

    Initial commit of tab

---
+++ Assets/ProjectTitan/Scripts/UI/UIComponent/TabGroup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Titan.UI
+{
+    public class TabGroup : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
