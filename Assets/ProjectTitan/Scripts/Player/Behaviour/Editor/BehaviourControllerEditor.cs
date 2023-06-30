f237b5e80ac2d65812f053796235a80624508c47
handakim25 <handakim25@gmail.com>
2023-06-30 21:06:57 +0900

    Implement BehaviourControllerEditor.

---
+++ Assets/ProjectTitan/Scripts/Player/Behaviour/Editor/BehaviourControllerEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Titan.Character.Player
+{
+    [CustomEditor(typeof(BehaviourController))]
+    public class BehaviourControllerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            BehaviourController controller = target as BehaviourController;
+
+            if(Application.isEditor && Application.isPlaying)
+            {
+                EditorGUILayout.Space();
+                var behaviour = controller.GetCurrentBehaviour();
+                var type = behaviour.GetType();
+                EditorGUILayout.LabelField("Current State", type.Name);
+            }
+        }
+    }
+}
