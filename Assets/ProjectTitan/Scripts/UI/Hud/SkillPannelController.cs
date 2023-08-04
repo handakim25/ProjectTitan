04ded8aaf09872e13ae56e8542ce4208010c3e47
handakim25 <handakim25@gmail.com>
2023-08-04 21:53:28 +0900

    Implement Skill Icon

---
+++ Assets/ProjectTitan/Scripts/UI/Hud/SkillPannelController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Titan
+{
+    public class SkillPannelController : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable skillParam;
+        [SerializeField] private FloatVariable hyperParam;
+
+        
+    }
+}
