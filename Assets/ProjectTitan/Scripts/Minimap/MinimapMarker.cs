090397f3a11d128cbae9341b05eb3849772f3172
handakim25 <handakim25@gmail.com>
2023-08-18 21:52:10 +0900

    Feature: minimap marker billboard

--- Assets/ProjectTitan/Scripts/Minimap/MinimapMarker.cs
+++ Assets/ProjectTitan/Scripts/Minimap/MinimapMarker.cs
@@ -15,7 +15,8 @@ namespace Titan.Character
 
         [SerializeField] private Sprite _maker;
         [SerializeField] private Color _color = new(1f, 1f, 1f);
-
+        [SerializeField] private bool _rotate = true;
+        [SerializeField] private bool _fitSize = false;
         private GameObject _markerGo;
 
         private void Start()
@@ -28,6 +29,23 @@ namespace Titan.Character
             var renderer = _markerGo.AddComponent<SpriteRenderer>();
             renderer.sprite = _maker;
             renderer.color = _color;
+
+            if(_fitSize)
+            {
+                // 가급적이면 이미지를 가공해서 사용하고 테스팅 용도로만 사용할 것.
+                var size = renderer.size;
+                _markerGo.transform.localScale = new(3f / size.x, 3f / size.y, 1f);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if(_rotate)
+            {
+                return;
+            }
+
+            _markerGo.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
         }
 
         public bool MarkerOn
