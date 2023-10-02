838ce3b3adf493e3dbd5bc58cc671e37ca7416ec
handakim25 <handakim25@gmail.com>
2023-10-02 22:30:15 +0900

    Implementing Stage Data

---
+++ Assets/ProjectTitan/Scripts/GamePlay/StageData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Titan.Resource;
+
+namespace Titan.Stage
+{
+    public class StageData : BaseData
+    {
+        
+    }
+}
---
+++ Assets/ProjectTitan/Scripts/GamePlay/StageDataObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Titan.Stage
+{
+    public class StageDataObject : ScriptableObject
+    {
+        public string SceneName;
+        public string StageName;
+        public SoundList BGM;
+    }
+}
