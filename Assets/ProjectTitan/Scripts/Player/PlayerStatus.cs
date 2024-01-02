fe93449992c223a074ed4756163b4db6c1d321c8
handakim25 <handakim25@gmail.com>
2024-01-02 21:10:58 +0900

    Feat: Energy 기능을 추가

--- Assets/ProjectTitan/Scripts/Player/PlayerStatus.cs
+++ Assets/ProjectTitan/Scripts/Player/PlayerStatus.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// @Refactor
+// Skill 관련을 묶어서 관리하는 클래스 필요
+// 해당 Class를 통해서 SKill Icon Controller와 관리할 것
+// 현재 시스템 상에서는 복잡한 Skill 구현을 할 때, Player Status, Skill Icon Controller, Skill Pannel Controller 등 수정되어야 할 곳이 많다.
+
 namespace Titan.Character.Player
 {
     public class PlayerStatus
@@ -25,6 +30,29 @@ namespace Titan.Character.Player
             set
             {
                 _maxHealth = value;
+                _isDirty = true;
+            }
+        }
+
+        // 현재 기준으로 궁극기만 Energy를 가지고 있다.
+        private float _maxEnergy;
+        public float MaxEnergy
+        {
+            get => _maxEnergy;
+            set
+            {
+                _maxEnergy = value;
+                _isDirty = true;
+            }
+        }
+
+        private float _curEnergy;
+        public float CurEnergy
+        {
+            get => _curEnergy;
+            set
+            {
+                _curEnergy = value;
                 _isDirty = true;
             }
         }
