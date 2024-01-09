02c9e73e2f717c7372c4772b72034becf56705f2
handakim25 <handakim25@gmail.com>
2024-01-09 13:51:49 +0900

    Docs: UIManager의 주석 작성

--- Assets/ProjectTitan/Scripts/UI/UIManager.cs
+++ Assets/ProjectTitan/Scripts/UI/UIManager.cs
@@ -13,9 +13,16 @@ using Titan.Graphics.PostProcessing;
 // 1. UIScene에서 UIManager를 호출하는 것은 좋아 보이지 않는다.
 // 2. CloseUI()를 호출하는 것이 아니라, UIScene에서 CloseUI를 호출하는 것도 좋아 보이지 않는다.
 // 실수할 가능성이 높다. 아니면 메소드의 의미가 명확하지 않는 것으로 보인다.
+// 3. 어쩌면 Input 처리도 각 Scene이 처리하는 것이 좋을 수도 있다. 가령, 겹치는 UI가 있을 경우에는 어떻게 처리할 것인가? 혹은 팝업 메뉴의 처리는? 가령 다른 게임에서는 toggle 시에 팝업을 닫는 방식으로 처리할 때도 있다.
+// 이런 케이스를 UIManager에서 다 처리하기에는 어려움이 있을 수 있다.
+// 각 Scene이 자신의 Input을 처리하고, UIManager에서는 각 Scene의 요청을 받아서 처리하는 것이 좋을 수도 있다.
+// 그렇다면 Input을 어떻게 넘겨줄 것인가?
 
 namespace Titan.UI
 {
+    /// <summary>
+    /// UI Scene을 관리하는 싱글톤 클래스
+    /// </summary>
     public sealed class UIManager : MonoSingleton<UIManager>, MainAction.IUIActions
     {
         [SerializeField] private InputActionAsset _inputAsset;
@@ -27,6 +34,12 @@ namespace Titan.UI
         [SerializeField] private UIScene inventoryUI;
         [SerializeField] private UIScene _questUI;
 
+        // @Refactor
+        // UI Scene을 각 Scene이 등록을 하거나 아니면 UI Manager가 찾아서 등록하거나 하는 식으로 수정할 것
+
+        /// <summary>
+        /// 등록된 UI Scene List. 창이 열릴 때 다른 창은 닫히게 된다.
+        /// </summary>
         [Space]
         [Tooltip("UI Scene List. 정상적으로 열리고 닫히기 위해서는 여기에 등록해야 한다.")]
         [SerializeField] private List<UIScene> _UIList = new List<UIScene>();
@@ -34,12 +47,15 @@ namespace Titan.UI
 
         /// <summary>
         /// 마우스 모드
-        /// 1. UI가 열려있을 경우
-        /// 2. Alt 키를 누른 경우
+        /// <para>1. UI가 열려있을 경우</para>
+        /// <para>2. Alt 키를 누른 경우</para>
         /// Focus를 잃었을 경우에는 마우스 모드가 해제된다.
         /// Foucs를 얻을 경우에는 다시 조건을 확인한다.
         /// </summary>
         private bool isMouseMode = false;
+
+        // @Refactor
+        // Input 처리 방식을 다시 생각해볼 것
 
         public event System.Action OnInteractEvent;
         public event System.Action<Vector2> OnInteractScrollEvent;
@@ -59,7 +75,7 @@ namespace Titan.UI
         }
 
         /// <summary>
-        /// Callback sent to all game objects when the player gets or loses focus.
+        /// Focus를 얻었을 때 isMouseMode에 따라 마우스 모드를 설정한다.
         /// </summary>
         /// <param name="focusStatus">The focus state of the application.</param>
         private void OnApplicationFocus(bool focusStatus)
@@ -188,19 +204,22 @@ namespace Titan.UI
 
         #endregion Input Callback
 
+        // Mouse Mode
+        // 1. HUD 상태 : 마우스 모드를 해제한다. Alt 키를 누르면 마우스 모드를 활성화한다.
+        // 2. UI 상태 : 마우스 모드를 설정한다.
+
+        /// <summary>
+        /// 마우스 모드를 활성화한다.
+        /// </summary>
         public void MouseModeOn()
         {
             Cursor.lockState = CursorLockMode.None;
             _action.Player.Look.Disable();
         }
 
-        // To-Do
-        // 상황에 맞춰서 커서 숨김 여부 처리
-        // Show
-        // 1. Alt 키 누른 동안
-        // 2. UI 활성화
-        // 그 외 상황
-        // Hide
+        /// <summary>
+        /// 마우스 모드를 비활성화한다.
+        /// </summary>
         public void MouseModeOff()
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -224,6 +243,10 @@ namespace Titan.UI
             return _raycastResults.Any(result => result.gameObject.GetComponent<Button>() != null);
         }
 
+        /// <summary>
+        /// UI Scene을 열거나 닫는다.
+        /// </summary>
+        /// <param name="targetScene"></param>
         private void ToggleUI(UIScene targetScene)
         {
             if(targetScene == null)
