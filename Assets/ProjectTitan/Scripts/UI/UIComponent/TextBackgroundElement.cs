db69f2b48d8f68c473064bd024e08b6c548cbdae
handakim25 <handakim25@gmail.com>
2024-01-22 22:41:44 +0900

    Feat: Textmeshprogui에 사용할 백그라운드 컴포넌트 추가
    현재 사용하지 않는다. 왜냐하면 VerticalGridLayout으로 해결했다.

---
+++ Assets/ProjectTitan/Scripts/UI/UIComponent/TextBackgroundElement.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+namespace Titan
+{
+    /// <summary>
+    /// 현재 사용하지 않는다. Vertical Layout Group을 사용하면 해결할 수 있다.
+    /// </summary>
+    [RequireComponent(typeof(RectTransform))]
+    [ExecuteAlways]
+    public class TextBackgroundElement : UIBehaviour, ILayoutElement
+    {
+        [SerializeField] private float _padding = 0;
+        TextMeshProUGUI text;
+        TextMeshProUGUI Text => text != null ? text : text = GetComponentInChildren<TextMeshProUGUI>();
+
+        public float minWidth => -1;
+        public float preferredWidth => Text != null ? Text.preferredWidth + _padding : -1;
+        public float flexibleWidth => -1;
+        public float minHeight => -1;
+        public float preferredHeight => Text != null ? Text.preferredHeight + _padding : -1;
+        public float flexibleHeight => -1;
+        public int layoutPriority => 1;
+
+        public void CalculateLayoutInputHorizontal() 
+        {
+            Debug.Log($"PreferredWidth: {preferredWidth}");
+            Debug.Log("CalculateLayoutInputHorizontal");
+        }
+        public void CalculateLayoutInputVertical() 
+        {
+            ScriptableObject test;
+            Debug.Log($"PreferredHeight: {preferredHeight}");
+            Debug.Log("CalculateLayoutInputVertical");
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            SetDirty();
+        }
+
+        protected override void OnDisable()
+        {
+            SetDirty();
+            base.OnDisable();
+        }
+
+        protected override void OnTransformParentChanged()
+        {
+            SetDirty();
+        }
+
+        protected override void OnBeforeTransformParentChanged()
+        {
+            SetDirty();
+        }
+
+        private void OnTransformChildrenChanged()
+        {
+            Debug.Log("OnTransformChildrenChanged");
+            SetDirty();
+        }
+
+        protected void SetDirty()
+        {
+            // TMPro_EventManager.TEXT_CHANGED_EVENT.Add
+            if(!IsActive())
+            {
+                return;
+            }
+            LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            SetDirty();
+        }
+#endif
+    }
+}
