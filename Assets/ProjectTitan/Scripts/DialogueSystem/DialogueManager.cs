645cb4700aef2a0d1a16a5bf4b592959df3da38e
handakim25 <handakim25@gmail.com>
2024-02-05 17:47:01 +0900

    Refactor: DialogueManager 변수를 정리하고 주석 추가
    - DialogueUI Property 변수명 변경. Property는 대문자로 시작하게 한다.
    - Property를 추가했기에 Property로 접근하도록 수정
    - StartDialogue 또한 다른 StartDialogue와 같은 Parameter를 받도록 수정

--- Assets/ProjectTitan/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/ProjectTitan/Scripts/DialogueSystem/DialogueManager.cs
@@ -22,6 +22,9 @@ namespace Titan.DialogueSystem
         [SerializeField] private DialogueUIController _dialogueUI;
         [Tooltip("다이얼로그가 종료되면 다음 다이얼로그가 바로 켜지지 않도록 텀을 둔다.")]
         [SerializeField] private float _dialogueInterval = 0.5f;
+        /// <summary>
+        /// 마지막 대화가 끝난 시간(Time.time)
+        /// </summary>
         private float _lastDialogueEndTime;
 
         // @After-Work
@@ -30,11 +33,17 @@ namespace Titan.DialogueSystem
         private Dictionary<string, DialogueObject> _dialogueObjectDic = new();
 
         private DialogueObject _currentDialogueObject;
+        /// <summary>
+        /// 현재 대화를 진행중인 대화 상대
+        /// </summary>
         private DialogInteractable _curDialogueInteractable;
         private DialogueNode _currentDialogueNode;
+        /// <summary>
+        /// Dialogue 종료 시에 호출되는 Callback
+        /// </summary>
         private System.Action _onDialogueEnd;
 
-        private string _curSpeaker => _curDialogueInteractable != null ? 
+        private string CurSpeaker => _curDialogueInteractable != null ? 
             _curDialogueInteractable.InteractText : null ?? _currentDialogueNode.SpeakerName;
         
         private ConditionEvaluator _conditionEvaluator;
@@ -62,17 +71,17 @@ namespace Titan.DialogueSystem
 
         private void Start() 
         {
-            if(_dialogueUI == null)
+            if(DialogueUI == null)
             {
                 DialogueUI = FindObjectOfType<DialogueUIController>(true);
             }
         }
 
-        public void StartDialogue(string DialogueID)
+        public void StartDialogue(string DialogueID, DialogInteractable interactable = null, System.Action OnDialogueEnd = null)
         {
             if(_dialogueObjectDic.TryGetValue(DialogueID, out var dialogueObject))
             {
-                StartDialogue(dialogueObject);
+                StartDialogue(dialogueObject, interactable, OnDialogueEnd);
             }
             else
             {
@@ -87,7 +96,7 @@ namespace Titan.DialogueSystem
             {
                 return;
             }
-            if(_dialogueUI == null)
+            if(DialogueUI == null)
             {
                 Debug.LogError("Dialogue UI is not exist");
                 return;
@@ -103,10 +112,14 @@ namespace Titan.DialogueSystem
                 return;
             }
 
-            _dialogueUI.GetComponent<DialogueUIScene>().OpenUI();
-            _dialogueUI.SetDialogue(_curSpeaker, _currentDialogueNode.SentenceText);
+            DialogueUI.GetComponent<DialogueUIScene>().OpenUI();
+            DialogueUI.SetDialogue(CurSpeaker, _currentDialogueNode.SentenceText);
         }
 
+        /// <summary>
+        /// 다음 대화를 가져온다.
+        /// </summary>
+        /// <returns>대화를 다 가져왔으면 null을 반환</returns>
         private DialogueNode GetNextDialogue()
         {
             if(_currentDialogueNode.NextNode != null)
@@ -117,55 +130,56 @@ namespace Titan.DialogueSystem
             return null;
         }
 
+        /// <summary>
+        /// 대화를 종료한다. UI를 종료하고 대화가 끝났음을 알린다.
+        /// </summary>
         private void EndDialogue()
         {
-            _dialogueUI.GetComponent<DialogueUIScene>().CloseUI();
+            DialogueUI.GetComponent<DialogueUIScene>().CloseUI();
 
             _currentDialogueObject = null;
             _lastDialogueEndTime = Time.time;
             _onDialogueEnd?.Invoke();
         }
 
-        private void FindUI()
-        {
-            _dialogueUI = FindObjectOfType<DialogueUIController>(true);
-            if(_dialogueUI != null)
-            {
-                _dialogueUI.OnNextDialogue += OnNextDialogueHandler;
-                _dialogueUI.OnDialougeEnd += OnDialogueEndHandler;
-                _dialogueUI.OnChoiceSelected += OnChoiceSelectedHandler;
-            }
-        }
-
         #region Callbacks
         
+        /// <summary>
+        /// 다음 대화를 출력한다.
+        /// </summary>
         private void OnNextDialogueHandler()
         {
-           if (_currentDialogueNode.Choices.Count > 0)
+            // 선택지가 존재하면 다음 대화를 출력하지 않는다.
+            // 선택지가 있다면 선택지를 선택해야된다.
+           if (HasChoices)
             {
                 return;
             } 
-            // show next dialogue
+
             var nextNode = GetNextDialogue();
             ProcessDialogue(nextNode);
         }
 
         /// <summary>
-        /// 선택지가 있으면 선택지를 출력한다.
+        /// 하나의 대화가 전부 출력됬을 때 호출되는 콜백. 선택지가 있으면 선택지를 출력한다.
         /// </summary>
         private void OnDialogueEndHandler()
         {
-            if (_currentDialogueNode.Choices.Count > 0)
+            if (HasChoices)
             {
                 // show choices
                 _conditionEvaluator ??= new ConditionEvaluator() {
                     QuestManager = QuestManager.Instance,
                     InventoryManager = InventoryManager.Instance,
                 };
-                var choiceTextList = _currentDialogueNode.Choices.Where(x => x.Condition.IsMet(_conditionEvaluator)).Select(x => x.ChoiceText).ToList();
-                _dialogueUI.ShowChoice(choiceTextList);
+                var choiceTextList = _currentDialogueNode.Choices.Where(x => x.Condition.IsMet(_conditionEvaluator))
+                    .Select(x => x.ChoiceText)
+                    .ToList();
+                DialogueUI.ShowChoice(choiceTextList);
             }
         }
+
+        private bool HasChoices => _currentDialogueNode.Choices.Count > 0;
 
         /// <summary>
         /// 선택지를 선택했을 때 처리한다.
@@ -175,6 +189,12 @@ namespace Titan.DialogueSystem
         {
             // show next dialogue
             var choiceNode = _currentDialogueNode.Choices.Find(x => x.ChoiceText == choiceStr);
+            if(choiceNode == null)
+            {
+                Debug.LogError("Choice is not exist");
+                EndDialogue();
+                return;
+            }
 
             var nextNodeID = choiceNode.NextNode;
             var nextNode = _currentDialogueObject.GetNode(nextNodeID);
--- Assets/ProjectTitan/Scripts/DialogueSystem/DialogueObject/Choice.cs
+++ Assets/ProjectTitan/Scripts/DialogueSystem/DialogueObject/Choice.cs
@@ -15,7 +15,7 @@ namespace Titan.DialogueSystem
         /// </summary>
         public string ChoiceText;
         /// <summary>
-        /// 선택지를 선택했을 때 다음 노드
+        /// 선택지를 선택했을 때 다음 노드 ID
         /// </summary>
         public string NextNode;
         /// <summary>
