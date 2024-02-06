a5a4eb3a321279c5eebb9d83c3fa60a84367ae3e
handakim25 <handakim25@gmail.com>
2024-02-06 09:45:56 +0900

    Feat: ItemType이 SubItemType을 가지도록 수정

--- Assets/ProjectTitan/Scripts/InventorySystem/Items/ItemObject.cs
+++ Assets/ProjectTitan/Scripts/InventorySystem/Items/ItemObject.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Titan.InventorySystem.Items
 {
-    [CreateAssetMenu(fileName = "New Item", menuName = "Inventory System/Items/New Item")]
+    [CreateAssetMenu(fileName = "NewItem", menuName = "Inventory System/Items/New Item")]
     public class ItemObject : ScriptableObject
     {
-        #region Variables
         public string ItemName;
-        public ItemType type;
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ItemType type = ItemType.None;
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ItemSubType subType = ItemSubType.None;
+        [JsonConverter(typeof(StringEnumConverter))]
         public ItemRarity rarity = ItemRarity.Common;
         public bool stackable;
 
         [Tooltip("인벤토리 UI에서 보여질 아이콘 이미지")]
         [SpritePreview]
+        [JsonIgnore]
         public Sprite icon;
 
         [TextArea(5,20)]
@@ -23,6 +29,12 @@ namespace Titan.InventorySystem.Items
 
         public Item data = new();
 
-        #endregion Variables
+        [ContextMenu("Show Json Data")]
+        public void ShowJsonData()
+        {
+            // Debug.Log(JsonUtility.ToJson(this, true));
+            var json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            Debug.Log(json);
+        }
     }
 }
--- Assets/ProjectTitan/Scripts/InventorySystem/Items/ItemType.cs
+++ Assets/ProjectTitan/Scripts/InventorySystem/Items/ItemType.cs
@@ -16,6 +16,23 @@ namespace Titan.InventorySystem.Items
         Misc,
     }
 
+    public enum ItemSubType
+    {
+        None,
+        // Weapon
+        Weapon_Sword,
+        Weapon_Spear,
+        // Acc
+        Acc_Ring,
+        Acc_Necklace,
+        // Consumable
+        Consumable_Potion,
+        // Food
+        Food_Meal,
+        // Misc
+        Misc_Material,
+        Misc_Trinket,
+    }
     public static class ItemTypeExtension
     {
         public static string ToText(this ItemType type)
@@ -28,6 +45,22 @@ namespace Titan.InventorySystem.Items
                 ItemType.Food => "음식",
                 ItemType.QuestItem => "퀘스트 아이템",
                 ItemType.Misc => "기타",
+                _ => "없음",
+            };
+        }
+
+        public static string ToText(this ItemSubType type)
+        {
+            return type switch
+            {
+                ItemSubType.Weapon_Sword => "검",
+                ItemSubType.Weapon_Spear => "창",
+                ItemSubType.Acc_Ring => "반지",
+                ItemSubType.Acc_Necklace => "목걸이",
+                ItemSubType.Consumable_Potion => "물약",
+                ItemSubType.Food_Meal => "음식",
+                ItemSubType.Misc_Material => "재료",
+                ItemSubType.Misc_Trinket => "잡동사니",
                 _ => "없음",
             };
         }
