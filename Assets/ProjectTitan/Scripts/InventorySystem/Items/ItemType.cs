f08cca65127d59d84861a614bbb5440981bf4074
handakim25 <handakim25@gmail.com>
2024-02-06 13:53:06 +0900

    Feat: ItemType에 Category를 확인할 수 있는 기능을 추가

--- Assets/ProjectTitan/Scripts/InventorySystem/Items/ItemType.cs
+++ Assets/ProjectTitan/Scripts/InventorySystem/Items/ItemType.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,19 +22,28 @@ namespace Titan.InventorySystem.Items
     {
         None,
         // Weapon
-        Weapon_Sword,
-        Weapon_Spear,
+        [ItemSubCategoryOf(ItemType.Weapon)]
+        Sword,
+        [ItemSubCategoryOf(ItemType.Weapon)]
+        Spear,
         // Acc
-        Acc_Ring,
-        Acc_Necklace,
+        [ItemSubCategoryOf(ItemType.Acc)]
+        Ring,
+        [ItemSubCategoryOf(ItemType.Acc)]
+        Necklace,
         // Consumable
-        Consumable_Potion,
+        [ItemSubCategoryOf(ItemType.Consumable)]
+        Potion,
         // Food
-        Food_Meal,
+        [ItemSubCategoryOf(ItemType.Food)]
+        Meal,
         // Misc
-        Misc_Material,
-        Misc_Trinket,
+        [ItemSubCategoryOf(ItemType.Misc)]
+        Material,
+        [ItemSubCategoryOf(ItemType.Misc)]
+        Trinket,
     }
+
     public static class ItemTypeExtension
     {
         public static string ToText(this ItemType type)
@@ -53,16 +64,73 @@ namespace Titan.InventorySystem.Items
         {
             return type switch
             {
-                ItemSubType.Weapon_Sword => "검",
-                ItemSubType.Weapon_Spear => "창",
-                ItemSubType.Acc_Ring => "반지",
-                ItemSubType.Acc_Necklace => "목걸이",
-                ItemSubType.Consumable_Potion => "물약",
-                ItemSubType.Food_Meal => "음식",
-                ItemSubType.Misc_Material => "재료",
-                ItemSubType.Misc_Trinket => "잡동사니",
+                ItemSubType.Sword => "검",
+                ItemSubType.Spear => "창",
+                ItemSubType.Ring => "반지",
+                ItemSubType.Necklace => "목걸이",
+                ItemSubType.Potion => "물약",
+                ItemSubType.Meal => "음식",
+                ItemSubType.Material => "재료",
+                ItemSubType.Trinket => "잡동사니",
                 _ => "없음",
             };
+        }
+
+        /// <summary>
+        /// ItemSubType이 ItemType과 같은지 확인
+        /// </summary>
+        /// <param name="subType"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsSubTypeOf(this ItemSubType subType, ItemType type)
+        {
+            return subType.GetItemType() == type;
+        }
+
+        /// <summary>
+        /// ItemSubType이 속한 ItemType을 반환
+        /// </summary>
+        /// <param name="subType"></param>
+        /// <returns>만약 속한 ItemType이 없다면 ItemType.None을 반환</returns>
+        public static ItemType GetItemType(this ItemSubType subType)
+        {
+            // @Fix
+            // 이 방식은 ItemSubType에 수식된 Attribute를 구하는 방식이다.
+            // Type subTypeInfo = subType.GetType();
+
+            // Get member info for the enum value
+            var memberInfo = typeof(ItemSubType).GetMember(subType.ToString())
+                .First();
+            var subTypeAttribute = (ItemSubCategoryOf)Attribute.GetCustomAttribute(memberInfo, typeof(ItemSubCategoryOf), false);
+            // 속한 Category가 존재하지 않은 경우
+            if(subTypeAttribute == null)
+            {
+                return ItemType.None;
+            }
+            return subTypeAttribute.Type;
+        }
+
+        /// <summary>
+        /// ItemType에 속한 ItemSubType들을 반환
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IEnumerable<ItemSubType> GetSubTypes(this ItemType type)
+        {
+            var values = Enum.GetValues(typeof(ItemSubType)).Cast<ItemSubType>();
+            return values.Where(subType => subType.GetItemType() == type);
+        }
+    }
+
+    // see https://stackoverflow.com/questions/26472062/mapping-enum-to-sub-enum
+    // see https://stackoverflow.com/questions/24823061/get-all-types-under-each-category-using-enum-in-c-sharp
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ItemSubCategoryOf : Attribute
+    {
+        public ItemType Type {get; private set;}
+        public ItemSubCategoryOf(ItemType type)
+        {
+            Type = type;
         }
     }
 }
